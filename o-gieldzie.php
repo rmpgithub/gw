d86074a14e53ff0f35779a0d0ecdc6b8a60a364d
rmpgitDellE5430 <rmpgit@gmail.com>
2020-05-20 09:14:43 +0200

    zmiana daty giełdy na 30 maja

--- o-gieldzie.php
+++ o-gieldzie.php
@@ -7,7 +7,7 @@
     <div class="container">
         <div class="row d-flex justify-content-center">
             <div class="col-12">
-                Zapraszamy Handlujących na giełdę towarowo-samochodową „Giełda Wschód”, która odbędzie się w każdy piątek w miejscowości Skołoszów, ul Przemyska 37, gmina Radymno, powiat jarosławski, bezpośrednio przy węźle „Przemyśl” – skrzyżowanie autostrady A4 z drogą krajową 77. Teren na którym od wielu lat, co roku odbywają się Agro Targi Wschód. Powierzchnia giełdy to ponad 20 ha. Giełda dla handlujących czynna będzie w godzinach 4.00-15.00. Profil giełdy: wszelka różnorodność, handel bazarowy, giełda samochodowa, sprzedaż maszyn rolniczych, środków do produkcji rolnej, artykułów ogrodniczych, szkółkarskich z wyłączeniem zwierząt.
+                Zapraszamy Handlujących na giełdę towarowo-samochodową „Giełda Wschód”, która odbędzie się w każdą Sobotę w miejscowości Skołoszów, ul Przemyska 37, gmina Radymno, powiat jarosławski, bezpośrednio przy węźle „Przemyśl” – skrzyżowanie autostrady A4 z drogą krajową 77. Teren na którym od wielu lat, co roku odbywają się Agro Targi Wschód. Powierzchnia giełdy to ponad 20 ha. Giełda dla handlujących czynna będzie w godzinach 4.00-15.00. Profil giełdy: wszelka różnorodność, handel bazarowy, giełda samochodowa, sprzedaż maszyn rolniczych, środków do produkcji rolnej, artykułów ogrodniczych, szkółkarskich z wyłączeniem zwierząt.
                 <br><br>
                 Ambicją Organizatora jest aby w krótkim czasie Giełda Wschód była wiodącą giełdą w południowo-wschodniej Polsce. 
             </div>
