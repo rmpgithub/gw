55627b8130f41677216527a08ed575bba98e49c1
rmpgitDellE5430 <rmpgit@gmail.com>
2020-06-02 10:21:35 +0200

    dodanie zdjęcia sferycznego do strony

--- kontakt.php
+++ kontakt.php
@@ -38,6 +38,9 @@
             <div class="map">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2569.273947253427!2d22.818527315711982!3d49.91243297940482!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x6f4c49a3fa080dae!2zR0lFxYFEQSBXU0NIw5NE!5e0!3m2!1spl!2spl!4v1591083537554!5m2!1spl!2spl" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
             </div>
+             <div class="map">
+                <iframe src="https://www.google.com/maps/embed?pb=!4v1591085568491!6m8!1m7!1sCAoSLEFGMVFpcE1IWU80Y1I3N3BQWDg1RjU5RUVBVUNuQlktWGt0ZGZXb2wzeWxQ!2m2!1d49.91259479560316!2d22.818329366841!3f119.66013953416817!4f-31.72970794355745!5f0.7820865974627469" width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+             </div>
         </div>
     </div>
 
