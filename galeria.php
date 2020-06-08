ed06272b23405b6fc2bc9c736171fea335c12ad3
rmpgitDellE5430 <rmpgit@gmail.com>
2020-06-08 18:03:00 +0200

    usuniecie z galarii zdjec

--- galeria.php
+++ galeria.php
@@ -6,23 +6,17 @@
 			
 			<div class="row">
 				<div class="col-xs-12-col-sm-6 col-md-4"><a href="./src/assets/img/galeria/1.JPG"><img class="Thumbnail" src="./src/assets/img/galeria/1.JPG" alt=""></a></div>
-				<div class="col-xs-12-col-sm-6 col-md-4"><a href="./src/assets/img/galeria/2.JPG"><img class="Thumbnail" src="./src/assets/img/galeria/2.JPG" alt=""></a></div>
 				<div class="col-xs-12-col-sm-6 col-md-4"><a href="./src/assets/img/galeria/3.JPG"><img class="Thumbnail" src="./src/assets/img/galeria/3.JPG" alt=""></a></div>
-			</div>
-
-			<div class="row">
 				<div class="col-xs-12-col-sm-6 col-md-4"><a href="./src/assets/img/galeria/4.JPG"><img class="Thumbnail" src="./src/assets/img/galeria/4.JPG" alt=""></a></div>
-				<div class="col-xs-12-col-sm-6 col-md-4"><a href="./src/assets/img/galeria/5.JPG"><img class="Thumbnail" src="./src/assets/img/galeria/5.JPG" alt=""></a></div>
-				<div class="col-xs-12-col-sm-6 col-md-4"><a href="./src/assets/img/galeria/6.JPG"><img class="Thumbnail" src="./src/assets/img/galeria/6.JPG" alt=""></a></div>
 			</div>
 
-			
-
 			<div class="row">
-				<div class="col-xs-12-col-sm-6 col-md-4"><a href="./src/assets/img/galeria/7.JPG"><img class="Thumbnail" src="./src/assets/img/galeria/7.JPG" alt=""></a></div>
+				<div class="col-xs-12-col-sm-6 col-md-4"><a href="./src/assets/img/galeria/6.JPG"><img class="Thumbnail" src="./src/assets/img/galeria/6.JPG" alt=""></a></div>
 				<div class="col-xs-12-col-sm-6 col-md-4"><a href="./src/assets/img/galeria/8.JPG"><img class="Thumbnail" src="./src/assets/img/galeria/8.JPG" alt=""></a></div>
 			    <div class="col-xs-12-col-sm-6 col-md-4"><a href="./src/assets/img/galeria/9.JPG"><img class="Thumbnail" src="./src/assets/img/galeria/9.JPG" alt=""></a></div>
 			</div>
+
+		
 
 			<div class="row">
 				<div class="col-xs-12-col-sm-6 col-md-4"><a href="./src/assets/img/galeria/10.JPG"><img class="Thumbnail" src="./src/assets/img/galeria/10.JPG" alt=""></a></div>
