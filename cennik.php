411a12c3504eaa6f2579161a5a7051a4af028502
rmpgit <rmpgit@gmail.com>
2020-07-16 13:44:44 +0200

    zmiana cennika

--- cennik.php
+++ cennik.php
@@ -16,22 +16,23 @@
                    <thead>
                       <tr> <th> L.p. <th> <th> Cena PLN
                    <tbody>
-                      <tr> <th> 1 <td> Opłata za stoisko handlowe 6mx6m bez rezerwacji <td> 60,00
+                      <tr> <th> 1 <td> Opłata za stoisko handlowe 6mx6m<td> 60,00
                       <tr> <th> 2 <td> Opłata za stoisko handlowe dla posiadaczy rezerwacji <td> 30,00
-                      <tr> <th> 3 <td> Rezerwacja stoiska handlowego 6mx6m na miesiąc <td> 75,00
-                      <tr> <th> 4 <td> Metr bieżący poza stoiskiem handlowym <td> 10,00
-                      <tr> <th> 5 <td> Sprzedaż samochodu <td> 20,00
-                      <tr> <th> 6 <td> Sprzedaż przyczepy, motocykla <td> 10,00
-                      <tr> <th> 7 <td> Opłata za sprzedaż z wózków <td> 10,00  
-                      <tr> <th> 8 <td> Gastronomia - samochód <td> 200,00  
-                      <tr> <th> 9 <td> Rękodzieło, antyki, wyprzedaż garażowa, starocia 1m^2 w wyznaczonej przez Organizatora strefie<td> 1,00 
-                      <tr> <th> 10 <td>Opłata za sprzątanie stoiska handlowego w przypadku nieprzestrzegania regulaminu <td>  10,00  
+                      
+                      <tr> <th> 3 <td> Metr bieżący poza stoiskiem handlowym <td> 10,00
+                      <tr> <th> 4 <td> Sprzedaż samochodu <td> 20,00
+                      <tr> <th> 5 <td> Sprzedaż przyczepy, motocykla <td> 10,00
+                      <tr> <th> 6 <td> Opłata za sprzedaż z wózków <td> 10,00  
+                      <tr> <th> 7 <td> Gastronomia - samochód <td> 200,00  
+                      <tr> <th> 8 <td> Rękodzieło, antyki, wyprzedaż garażowa, starocia 1m^2 w wyznaczonej przez Organizatora strefie<td> 1,00 
+                      <tr> <th> 9 <td>Opłata za sprzątanie stoiska handlowego w przypadku nieprzestrzegania regulaminu <td>  10,00  
                       <!-- <tr> <th> 7 <td> Catering- lody <td> 250,00 -->
                     <!--   <tr> <th> 2 <td> Opłata za sprzątanie stoiska handlowego <td> 10,00 -->
                      
                 </table>
                 
-                
+                <br><br>Istnieje możliwość rezerwacji miejsca handlowego do końca listopada 2020r.
+
 
                 <br><br>Wjazd na giełdę odbywać się będzie według kolejności przyjazdu Handlujących przy wjeździe nr. 1. Miejsca handlowe zostaną wskazane przez obsługę giełdy zgodnie z kolejnością stoisk. O zmianie formy wjazdu na teren Giełdy Wschód poinformujemy Państwa na 7 dni przed zmianą na stronie internetowej  lub w czasie pobierania opłaty targowej.
                 <br><br> Ilość miejsc przeznaczonych pod gastronomię jest ograniczona- decyduje Organizator- zamiar przyjazdu proszę zgłosić telefonicznie. Organizator nie gwarantuje podłączenia do prądu.
