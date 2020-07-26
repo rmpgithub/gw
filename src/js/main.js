ef2536d5226ca49dcf7b2a4747ffdf15616fad9d
rmpgitDellE5430 <rmpgit@gmail.com>
2020-07-26 17:52:25 +0200

    ZMIANA DATY na 2 sierpnia

--- src/js/main.js
+++ src/js/main.js
@@ -31,7 +31,7 @@ $(document).ready(function () {
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("July 25, 2020 05:00:00").getTime();
+var countDownDate = new Date("AUGUST 2, 2020 05:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
