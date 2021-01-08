fc87463a5aae9cdfc454a2dd61431a85c274a887
rmpgitDellE5430 <rmpgit@gmail.com>
2021-01-08 20:07:00 +0100

    poprawienie aktualnosci

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 
 </div>
  -->
-<div class="dla_firm">PRZYPOMINAMY!<br>
+<!-- <div class="dla_firm">PRZYPOMINAMY!<br>
 
 NA TERENIE GIEŁDY WSCHÓD OBOWIĄZUJE BEZWZGLĘDNY NAKAZ ZAKRYWANIA UST I NOSA, A TAKŻE DEZYNFEKCJA RĄK I ZACHOWANIE DYSTANSU SPOŁECZNEGO.<br>
 Giełda Wschód jest czynna od godziny 5.00 z zachowaniem wszelkich obostrzeń i zaleceń dotyczących reżimu sanitarnego. Zarówno kupujących jak i handlujących prosimy o zapoznanie się z aktualnymi obostrzeniami i bezwzględne stosowanie się do zaleceń reżimu sanitarnego.
@@ -29,7 +29,7 @@ https://www.gov.pl/web/koronawirus<br>
  
 Zgodnie z ostatnimi wytycznymi na Giełdzie Wschód obowiązuje zakaz aranżacji miejsc przeznaczonych do spożywania posiłków.<br>
     
-    </div>
+    </div> -->
 <!-- <div class="container gallery gallery-index">
             
             
@@ -86,22 +86,36 @@ Zgodnie z ostatnimi wytycznymi na Giełdzie Wschód obowiązuje zakaz aranżacji
     </div>
 
     <div class="row">
+        <div class="dla_firm">
+
+NA TERENIE GIEŁDY WSCHÓD OBOWIĄZUJE BEZWZGLĘDNY NAKAZ ZAKRYWANIA UST I NOSA, A TAKŻE DEZYNFEKCJA RĄK I ZACHOWANIE DYSTANSU SPOŁECZNEGO.<br>
+Giełda Wschód jest czynna od godziny 5.00 z zachowaniem wszelkich obostrzeń i zaleceń dotyczących reżimu sanitarnego. Zarówno kupujących jak i handlujących prosimy o zapoznanie się z aktualnymi obostrzeniami i bezwzględne stosowanie się do zaleceń reżimu sanitarnego.
+Lista obostrzeń dostępna na stronie:
+https://www.gov.pl/web/koronawirus<br>
+ 
+Zgodnie z ostatnimi wytycznymi na Giełdzie Wschód obowiązuje zakaz aranżacji miejsc przeznaczonych do spożywania posiłków.<br>
+    
+        </div>
+    </div>    
+
+
+    <!-- <div class="row">
         <div class="col-12 col-lg-6">
-            <div class="item">
+            <div class="item"> -->
                 <!-- <div class="thumb">
                     <img src="./src/assets/img/1.jpg" alt="1">
                 </div> -->
                 <!-- <p class="date">10-05-2020</p>
                 <h2 class="heading-2"><a href="aktualnosc-pierwsza.php">Artykuł 1</a></h2> -->
-                <p class="text">Giełda Wschód to:<br>
+                <!-- <p class="text">Giełda Wschód to:<br>
                 - giełda staroci (pchli targ),<br>
                 - aukcja maszyn używanych,<br>
                 - sprzedaż samochodów amerykańskich,<br>
                 
-                </p>
+                </p> -->
                 <!-- <a href="aktualnosc-pierwsza.php" class="button-outline">DOWIEDZ SIĘ WIĘCEJ <i class="fa fa-chevron-right ml-2"></i></a> -->
-            </div>
-        </div>
+            <!-- </div>
+        </div> -->
         <!-- <div class="col-12 col-lg-4">
             <div class="item">
                 <div class="thumb">
@@ -114,18 +128,18 @@ Zgodnie z ostatnimi wytycznymi na Giełdzie Wschód obowiązuje zakaz aranżacji
                 <a href="#" class="button-outline">DOWIEDZ SIĘ WIĘCEJ <i class="fa fa-chevron-right ml-2"></i></a>
             </div>
         </div> -->
-        <div class="col-12 col-lg-6">
-            <div class="item">
+        <!-- <div class="col-12 col-lg-6">
+            <div class="item"> -->
                 <!-- <div class="thumb">
                     <img src="./src/assets/img/1.jpg" alt="1">
                 </div> -->
                 <!-- <p class="date">10-05-2020</p>
                 <h2 class="heading-2"><a href="#">Artykuł 1</a></h2> -->
-                <p class="text">Handel na Giełdzie Wschód możliwy jest bez wcześniejszej rezerwacji miejsca.</p>
-                <!-- <a href="#" class="button-outline">DOWIEDZ SIĘ WIĘCEJ <i class="fa fa-chevron-right ml-2"></i></a> -->
-            </div>
+               <!--  <p class="text">Handel na Giełdzie Wschód możliwy jest bez wcześniejszej rezerwacji miejsca.</p>
+                <a href="#" class="button-outline">DOWIEDZ SIĘ WIĘCEJ <i class="fa fa-chevron-right ml-2"></i></a> -->
+            <!-- </div>
         </div>
-    </div>
+    </div> -->
 </section>
 
 
