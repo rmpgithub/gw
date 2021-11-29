d7e0c675b297ebea1989504c65dbf12480d69300
rmpgit <rmpgit@gmail.com>
2021-11-29 14:13:40 +0100

    dodanie meta dla fb

--- header.php
+++ header.php
@@ -7,6 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
 
+    <meta name="facebook-domain-verification" content="k99luxd9aw6er9tvklqdxs6pvgkq34" />
+    
     <meta name="description" content="Giełda Wschód to największa towarowo-samochodowa giełda w Polsce położona bezpośrednio przy skrzyżowaniu drogi krajowej 77 i autostrady A4 (węzeł Przemyśl). Powierzchnia Giełdy Wschód to ponad 50ha. " />
 
 <!--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>-->
