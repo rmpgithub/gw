a036540a9e8322d2f5c1287743b4a67e0170be0b
rmpgit <rmpgit@gmail.com>
2021-12-03 20:56:30 +0100

    zmiany w cenniku

--- cennik.php
+++ cennik.php
@@ -26,17 +26,17 @@
                       <!-- <tr> <th> 7 <td> Gastronomia do uzgodnienia z Organizatorem  <td>  200,00 -->
                       <tr> <th> 7 <td> Rękodzieło, antyki, wyprzedaż garażowa, starocia w wyznaczonej przez Organizatora strefie - jeden metr kwadratowy<td> 1,00 
                       <tr> <th> 8 <td>Opłata za sprzątanie stoiska handlowego w przypadku nieprzestrzegania regulaminu <td>  10,00  
-                      <tr> <th> 9 <td>Handlujący zbierają po sobie śmieci. Nie przestrzeganie tego przepisu spowoduje nie wpuszczenie na teren giełdy. <td>     
+                      <!-- <tr> <th> 9 <td>Handlujący zbierają po sobie śmieci. Nie przestrzeganie tego przepisu spowoduje nie wpuszczenie na teren giełdy. <td>    -->  
                       <!-- <tr> <th> 7 <td> Catering- lody <td> 250,00 -->
-                    <!--   <tr> <th> 2 <td> Opłata za sprzątanie stoiska handlowego <td> 10,00 -->
+                      <tr> <th> 9 <td> Opłata za sprzątanie stoiska handlowego <td> 10,00
                      
                 </table>
 
 
                 
-                <br><br>Istnieje możliwość rezerwacji miejsca handlowego do końca listopada 2021r.
+                <br><br>Istnieje możliwość rezerwacji miejsca handlowego do końca listopada 2022r.
 
-                <br><br>Po uzgodnieniu z Organizatorem i otrzymaniu zgody na piśmie, istnieje możliwość utwardzenia powierzchni wynajętej do końca Listopada 2021 przez Handlującego. 
+                <br><br>Po uzgodnieniu z Organizatorem i otrzymaniu zgody na piśmie, istnieje możliwość utwardzenia powierzchni wynajętej do końca Listopada 2022 przez Handlującego. 
 
 
                 <br><br>Wjazd na giełdę odbywać się będzie według kolejności przyjazdu Handlujących przy wjeździe nr. 1. Miejsca handlowe zostaną wskazane przez obsługę giełdy zgodnie z kolejnością stoisk. O zmianie formy wjazdu na teren Giełdy Wschód poinformujemy Państwa na 7 dni przed zmianą na stronie internetowej  lub w czasie pobierania opłaty targowej.
