4b4c6c4d199d3ae30b1f32b5a508fae2b6dbbb51
rmpgit <rmpgit@gmail.com>
2022-09-25 11:28:04 +0200

    dodanie nowych zdjec i slidere

--- slider.php
+++ slider.php
@@ -3,8 +3,8 @@
         <div class="col-12 p-0">
             <div class="slider-index">
                 <div class="slide"><img src="./src/assets/img/slider/1.jpg" alt="1"></div>
-                <div class="slide"><img src="./src/assets/img/slider/2.jpg" alt="2"></div>
-                <!-- <div class="slide"><img src="./src/assets/img/slider/3.jpg" alt="3"></div>
+                <!--<div class="slide"><img src="./src/assets/img/slider/2.jpg" alt="2"></div>
+                 <div class="slide"><img src="./src/assets/img/slider/3.jpg" alt="3"></div>
                 <div class="slide"><img src="./src/assets/img/slider/4.jpg" alt="4"></div>
                 <div class="slide"><img src="./src/assets/img/slider/5.jpg" alt="5"></div> -->
                 <!--<div class="slide"><img src="./src/assets/img/slider/4.png" alt="4"></div> -->
