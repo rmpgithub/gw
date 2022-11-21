07acb3fe22b796e195a11c2f2ca3b65f88f33456
rmpgit <rmpgit@gmail.com>
2022-11-21 15:39:25 +0100

    usuniecie info o cennach

--- cennik.php
+++ cennik.php
@@ -7,12 +7,12 @@
 <div class="forExhibitors-regulations rule-index">
     <div class="container">
         <div class="row d-flex justify-content-center">
-            <div class="col-12">
+            <!-- <div class="col-12">
                 Na terenie Giełdy Wschód obowiązują następujące opłaty:<br><br>
-            </div>
+            </div> -->
             <div class="col-12">
 
-                <table>
+                <!-- <table>
                    <thead>
                       <tr> <th> L.p. <th> <th> Cena PLN
                    <tbody>
@@ -23,24 +23,22 @@
                       <tr> <th> 4 <td> Sprzedaż samochodu <td> 10,00
                       <tr> <th> 5 <td> Sprzedaż przyczepy, motocykla <td> 10,00
                       <tr> <th> 6 <td> Opłata za sprzedaż z wózków <td> 20,00  
-                      <!-- <tr> <th> 7 <td> Gastronomia do uzgodnienia z Organizatorem  <td>  200,00 -->
+                      <tr> <th> 7 <td> Gastronomia do uzgodnienia z Organizatorem  <td>  200,00
                       <tr> <th> 7 <td> Rękodzieło, antyki, wyprzedaż garażowa, starocia w wyznaczonej przez Organizatora strefie - jeden metr kwadratowy<td> 1,00 
                       <tr> <th> 8 <td>Opłata za sprzątanie stoiska handlowego w przypadku nieprzestrzegania regulaminu <td>  10,00  
-                      <!-- <tr> <th> 9 <td>Handlujący zbierają po sobie śmieci. Nie przestrzeganie tego przepisu spowoduje nie wpuszczenie na teren giełdy. <td>    -->  
-                      <!-- <tr> <th> 7 <td> Catering- lody <td> 250,00 -->
-                      <!-- <tr> <th> 9 <td> Opłata za sprzątanie stoiska handlowego <td> 10,00 -->
+                      
                      
-                </table>
+                </table> -->
 
 
                 
-                <br><br>Istnieje możliwość rezerwacji miejsca handlowego do końca listopada 2022r.
+                <!-- <br><br>Istnieje możliwość rezerwacji miejsca handlowego do końca listopada 2022r.
 
                 <br><br>Po uzgodnieniu z Organizatorem i otrzymaniu zgody na piśmie, istnieje możliwość utwardzenia powierzchni wynajętej do końca Listopada 2022 przez Handlującego. 
 
 
                 <br><br>Wjazd na giełdę odbywać się będzie według kolejności przyjazdu Handlujących przy wjeździe nr. 1. Miejsca handlowe zostaną wskazane przez obsługę giełdy zgodnie z kolejnością stoisk. O zmianie formy wjazdu na teren Giełdy Wschód poinformujemy Państwa na 7 dni przed zmianą na stronie internetowej  lub w czasie pobierania opłaty targowej.
-                <!-- <br><br> Ilość miejsc przeznaczonych pod gastronomię jest ograniczona- decyduje Organizator- zamiar przyjazdu proszę zgłosić telefonicznie. Organizator nie gwarantuje podłączenia do prądu. -->
+                <br><br> Ilość miejsc przeznaczonych pod gastronomię jest ograniczona- decyduje Organizator- zamiar przyjazdu proszę zgłosić telefonicznie. Organizator nie gwarantuje podłączenia do prądu. -->
  
             </div>
             
